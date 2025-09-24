cd5416905f4e41fc3e225fd987f32a6b2634e705
chumv <chumva.forever@gmail.com>
2025-09-24 16:22:44 +0300

    Update map sample to target 35

--- OsmAnd-map-sample/app/src/main/java/net/osmand/library/sample/SampleApplication.java
+++ OsmAnd-map-sample/app/src/main/java/net/osmand/library/sample/SampleApplication.java
@@ -4,7 +4,6 @@ import net.osmand.plus.OsmandApplication;
 
 // via SampleApplication we get access to OsmandApplication and all its settings and helpers
 public class SampleApplication extends OsmandApplication {
-
 	@Override
 	public void onCreate() {
 		super.onCreate();
--- OsmAnd-map-sample/app/src/main/java/net/osmand/library/sample/SimpleMapActivity.java
+++ OsmAnd-map-sample/app/src/main/java/net/osmand/library/sample/SimpleMapActivity.java
@@ -1,20 +1,35 @@
 package net.osmand.library.sample;
 
+import static net.osmand.plus.utils.InsetsUtils.InsetSide.BOTTOM;
+import static net.osmand.plus.utils.InsetsUtils.InsetSide.LEFT;
+import static net.osmand.plus.utils.InsetsUtils.InsetSide.RIGHT;
+import static net.osmand.plus.utils.InsetsUtils.InsetSide.TOP;
+
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import net.osmand.plus.OsmandApplication;
+import net.osmand.plus.activities.OsmandActionBarActivity;
 import net.osmand.plus.activities.RestartActivity;
+import net.osmand.plus.helpers.AndroidUiHelper;
+import net.osmand.plus.utils.AndroidUtils;
+import net.osmand.plus.utils.InsetsUtils;
+import net.osmand.plus.utils.InsetsUtils.InsetSide;
 import net.osmand.plus.views.MapViewWithLayers;
 import net.osmand.plus.views.OsmandMapTileView;
 
-public class SimpleMapActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class SimpleMapActivity extends OsmandActionBarActivity {
 
 	private OsmandApplication app;
 	private OsmandMapTileView mapTileView;
@@ -31,12 +46,10 @@ public class SimpleMapActivity extends AppCompatActivity {
 		mapTileView = app.getOsmandMap().getMapView();
 		mapTileView.setupRenderingView();
 
-		ActionBar actionBar = getSupportActionBar();
-		if (actionBar != null) {
-			actionBar.setTitle("Simple map");
-			actionBar.setDisplayHomeAsUpEnabled(true);
-			actionBar.setDisplayShowHomeEnabled(true);
-		}
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		toolbar.setTitle("Simple map");
+		toolbar.setNavigationIcon(AndroidUtils.getNavigationIconResId(app));
+		toolbar.setNavigationOnClickListener(v -> onBackPressed());
 
 		CompoundButton openglSwitch = findViewById(R.id.opengl_switch);
 		openglSwitch.setChecked(app.getSettings().USE_OPENGL_RENDER.get());
@@ -48,6 +61,23 @@ public class SimpleMapActivity extends AppCompatActivity {
 		//set start location and zoom for map
 		mapTileView.setIntZoom(14);
 		mapTileView.setLatLon(52.3704312, 4.8904288);
+	}
+
+	@Override
+	public void updateStatusBarColor() {
+		int color = AndroidUtils.getColorFromAttr(this, android.R.attr.colorPrimary);
+		if (color != -1) {
+			AndroidUiHelper.setStatusBarColor(this, color);
+		}
+	}
+
+	@Override
+	public void onContentChanged() {
+		super.onContentChanged();
+
+		View root = findViewById(R.id.root);
+		List<InsetSide> sides = Arrays.asList(LEFT, TOP, RIGHT, BOTTOM);
+		InsetsUtils.setWindowInsetsListener(root, new HashSet<>(sides));
 	}
 
 	@Override
